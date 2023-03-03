5c30acc916cfb1ef9f41253395304309b7238818
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-03-03 15:47:44 +0500

    responsive

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let logo = document.querySelector(".logo")
+let menuright = document.querySelector("menuRight")
 logo.onclick = () => {
     logo.style.display = "none"
 }
 console.log(logo);
+let sabzi = document.querySelector(".sabzi")
+sabzi.onclick = () => {
+    sabzi.style.display = "none"
+    menuright.style.display = "block"
+}
+
--- index.md
+++ index.md
@@ -1,1 +1,3 @@
+### 28.02.2023 boshladim Lada Largus
 ### 03.03.2023 13:53 desktop varianti tugadi
+### 03.03.2023 15:33 responsive tugadi
