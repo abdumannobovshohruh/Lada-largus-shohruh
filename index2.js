6efa2c7517bff2ce85975800ed89ae0995f77b97
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-03-04 12:26:10 +0500

    tugadi

--- index2.js
+++ index2.js
@@ -2,16 +2,22 @@ let body = document.querySelector("body")
 let flex = document.createElement("div")
 flex.classList.add("flex")
 body.append(flex)
-let arr = ["div", "div"]
+// let brr = ["img" , "img"]
+let arr = ["div", "div", ] /* "div" , "div" */
 arr.forEach((elem) => {
     let box = document.createElement(elem)
+    let start = document.createElement("h1")
     box.classList.add("box")
+    start.innerHTML = "<p> VESTA SW CROSS </p>"
     flex.appendChild(box)
     let photo = document.createElement("img")
-photo.src = "https://foto.carexpert.ru/img/foto1680/vaz/vazve115.jpg"
-photo.width = "500"
-photo.height = '300'
-photo.alt = 'good'
+    photo.src = "https://foto.carexpert.ru/img/foto1680/vaz/vazve115.jpg"
+    photo.width = "500"
+    photo.height = '300'
+    photo.alt = 'good'
+    start.style.color = "pink"
+    start.style.textAlign = "center"
 box.append(photo)
-// box.innerHTML = <h1 >helo</h1>
+box.append(start)
 })
+body.style.backgroundColor = "brown"
