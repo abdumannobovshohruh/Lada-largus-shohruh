918f106dcc1e09acb5c4d163b2094c11332b0441
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-03-04 14:42:15 +0500

    tugadi e

--- index2.js
+++ index2.js
@@ -9,7 +9,9 @@ arr.forEach((elem) => {
     let start = document.createElement("h1")
     box.classList.add("box")
     start.innerHTML = "<p> VESTA SW CROSS </p>"
+    // ism.innerHTML = "<h1> Hello my world </h1>"
     flex.appendChild(box)
+    // let ism = document.createElement("h1")
     let photo = document.createElement("img")
     photo.src = "https://foto.carexpert.ru/img/foto1680/vaz/vazve115.jpg"
     photo.width = "500"
@@ -19,5 +21,8 @@ arr.forEach((elem) => {
     start.style.textAlign = "center"
 box.append(photo)
 box.append(start)
+// box.append(ism)
 })
 body.style.backgroundColor = "brown"
+
+
